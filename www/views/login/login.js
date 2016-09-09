3bef2f864760f729966964bb14b9c014e8c47386
jbwg312 <jbwg312@gmail.com>
2016-09-09 15:39:27 -0600

    nothing new probably

--- www/views/login/login.js
+++ www/views/login/login.js
@@ -26,7 +26,6 @@ angular.module('App').controller('loginController', function ($scope, $state,$co
     }
   };
 
-/* SEEMS NOT WORKING WELL
 
   $scope.loginWithGoogle =  function(){
   var provider = new firebase.auth.GoogleAuthProvider();
@@ -51,7 +50,7 @@ angular.module('App').controller('loginController', function ($scope, $state,$co
   ;
   };
 
-*/
+
 
 /* SEEMS NOT WORKING WELL
   $scope.loginWithFacebook =  function(){
@@ -78,7 +77,7 @@ angular.module('App').controller('loginController', function ($scope, $state,$co
   };
   */
 
-/* SEEMS NOT WORKING WELL
+
   $scope.loginWithTwitter =  function(){
     var provider = new firebase.auth.FacebookAuthProvider();
 
@@ -101,6 +100,6 @@ angular.module('App').controller('loginController', function ($scope, $state,$co
 });
   ;
   };
-*/
+
 
 });
